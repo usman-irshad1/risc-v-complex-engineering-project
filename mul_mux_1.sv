5fe9d2fbca25d388d4a18eb5553ec643099dd5bb
Mohammad Usman Irshad <217554659+usman-irshad1@users.noreply.github.com>
2026-04-14 12:00:00 +0500

    feat(module): Add Verilog module mul_mux_1.sv

---
+++ mul_mux_1.sv
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2025 12:57:34 PM
+// Design Name: 
+// Module Name: mul_mux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mul_mux  #(parameter ins_width = 32, ins_depth=19, pc_width = 32, opcode_width=7, r_width=5, fun1=3, fun2=7, immediate_len=12)
+( 
+input logic [opcode_width-1:0] opcode,
+input logic [3:0 ]ALU_control,
+output logic [ins_width-1:0] alu_result_final,
+input logic [ins_width-1:0] alu_1,mul_1      
+
+ );
+always_comb
+begin
+if (opcode==7'b1100011)begin
+    if (ALU_control==4'b1000||ALU_control==4'b1001)
+begin
+alu_result_final=mul_1;
+end
+end
+else
+begin
+alu_result_final=alu_1;
+end
+end
+endmodule
